06fc6cd995bef5343522ba93353d85d8e7ef2a55
Naod Alemu <60686121+naodalemu@users.noreply.github.com>
2023-11-03 17:09:27 +0300

    Temporary commiting to go out but started the contact page big time.

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -4,8 +4,27 @@ function Contact() {
     return (
         <div className={classes.contactSection}>
             <div id="contact" className={classes.contactMargin}></div>
-            <h1>Contact Me</h1>
-            <div className={classes.contactContainer}></div>
+            <h1 className={classes.contactSectionName}>Contact Me</h1>
+            <form className={classes.contactContainer}>
+                <div className={classes.firstContactInfo}>
+                    <div className={classes.nameContainer}>
+                        <label htmlFor="senderName" className={classes.senderNameLabel}>Full Name</label>
+                        <input type="text" id="senderName" className={`${classes.senderName} ${classes.contactInput}`} name="senderName" />
+                    </div>
+                    <div className={classes.emailContainer}>
+                        <label htmlFor="senderEmail" className={classes.senderEmailLabel}>Email Address</label>
+                        <input type="email" id="senderEmail" className={`${classes.senderEmail} ${classes.contactInput}`} name="senderEmail" />
+                    </div>
+                    <div className={classes.subjectContainer}>
+                        <label htmlFor="messageSubject" className={classes.messageSubjectLabel}>Subject</label>
+                        <input type="text" id="messageSubject" className={`${classes.messageSubject} ${classes.contactInput}`} name="messageSubject" />
+                    </div>
+                </div>
+                <div className={classes.lastContactInfo}>
+                    <textarea id="contactMessage" className={classes.contactMessage} name="contactMessage" placeholder="Write your message here..."></textarea>
+                    <button className={classes.contactButton}>Send</button>
+                </div>
+            </form>
         </div>
     )
 }
