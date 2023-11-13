7c4faf9bc9ef56028ac49c04a7e88e97d85e81be
Naod Alemu <60686121+naodalemu@users.noreply.github.com>
2023-11-14 00:38:19 +0300

    Changing the color and font of the warnings of the contact section

--- src/components/UI/FailedEmail.jsx
+++ src/components/UI/FailedEmail.jsx
@@ -3,7 +3,7 @@ import classes from "./SuccessfulAndFailed.module.css"
 function FailedEmail() {
     return (
         <div className={classes.messageContainer}>
-            <div className={classes.message}>Failed! Please check your internet connection</div>
+            <div className={classes.message} style={{ color: "firebrick" }}>Failed! Please check your internet connection</div>
         </div>
     )
 }
--- src/components/UI/SuccessfulEmail.jsx
+++ src/components/UI/SuccessfulEmail.jsx
@@ -3,7 +3,7 @@ import classes from "./SuccessfulAndFailed.module.css"
 function SuccessfulEmail() {
     return (
         <div className={classes.messageContainer}>
-            <div className={classes.message}>Successfully Sent</div>
+            <div className={classes.message} style={{ color: "green" }}>Successfully Sent</div>
         </div>
     )
 }
